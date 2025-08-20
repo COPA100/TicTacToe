6d5a0f87f6ee7824128ed2a4dc7b6bf8a2636817
COPA100 <colinpark100@gmail.com>
2025-08-19 23:20:34 -0400

    some of the logic completed

--- app.js
+++ app.js
@@ -27,14 +27,49 @@ const gameboardFactory = function () {
 const gameRunner = function () {
     const board = gameboardFactory();
     let currentPlayer;
+    let p1, p2;
     const initializeGame = (p1name, p2name) => {
-        const p1 = playerFactory(p1name, "X");
-        const p2 = playerFactory(p2name, "O");
+        p1 = playerFactory(p1name, "X");
+        p2 = playerFactory(p2name, "O");
         currentPlayer = p1;
-        resetGameboard();
+        board.resetGameboard();
     };
-    const makeMove = () => {
-        
-    }
-    return {initializeGame};
+    const checkWin = () => {
+        const winArray = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+
+        const curBoard = board.getGameboard();
+
+        for (let i = 0; i < winArray.length; i++) {
+            if (
+                curBoard[winArray[i][0]] &&
+                curBoard[winArray[i][0]] === curBoard[winArray[i][1]] &&
+                curBoard[winArray[i][1]] === curBoard[winArray[i][2]]
+            ) {
+                return true;
+            }
+        }
+
+        return false;
+    };
+    const makeMove = (index) => {
+        board.setMarker(index, currentPlayer.mark());
+        if (checkWin()) {
+            return currentPlayer;
+        }
+        if (currentPlayer === p1) {
+            currentPlayer = p2;
+        } else {
+            currentPlayer = p1;
+        }
+    };
+    return { initializeGame, makeMove, checkWin };
 };
